4802adebc8991aad66fce3d1728e9cdabcd7e0e4
Polskivus <polako_irun@icloud.com>
2023-02-07 09:21:06 +0100

    CREADA LA CONEXION

--- src/objetos/Clientes.java
+++ src/objetos/Clientes.java
@@ -20,6 +20,51 @@ public class Clientes {
 		this.direccion = direccion;
 		this.localidad = localidad;
 	}
+
+	public String getDni() {
+		return dni;
+	}
+
+	public void setDni(String dni) {
+		this.dni = dni;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getApellido() {
+		return apellido;
+	}
+
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+
+	public String getDireccion() {
+		return direccion;
+	}
+
+	public void setDireccion(String direccion) {
+		this.direccion = direccion;
+	}
+
+	public String getLocalidad() {
+		return localidad;
+	}
+
+	public void setLocalidad(String localidad) {
+		this.localidad = localidad;
+	}
+	
+	public String toString() {
+		return "Clientes [dni=" + dni + ", nombre=" + nombre + ", apellido=" + apellido + ", direccion=" + direccion
+				+ ", localidad=" + localidad + "]";
+	}
 	
 }
 
