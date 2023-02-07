25d9f94c773064d9bb55d97feeba2ea881bd6079
Polskivus <polako_irun@icloud.com>
2023-02-07 09:57:48 +0100

    Gestor agencia creado, para ir implementando el menu

---
+++ src/gestores/GestorAgencia.java
@@ -0,0 +1,51 @@
+package gestores;
+
+import java.util.Scanner;
+
+import menu.Menu;
+
+public class GestorAgencia {
+
+	Scanner sc = new Scanner(System.in);
+
+	public void run() throws ClassNotFoundException {
+
+		int opc;
+
+		do {
+			Menu.menuPrincipal();
+
+			opc = Integer.parseInt(sc.nextLine());
+
+			while (opc < Menu.SALIR || opc > Menu.GESTION_RESERVAS) {
+				
+				System.out.println("!Error¡ Introduce un valor valido");
+
+				Menu.menuPrincipal();
+
+				opc = Integer.parseInt(sc.nextLine());
+			}
+			
+			switch (opc) {
+			case Menu.GESTION_CLIENTES:
+				
+				break;
+
+			case Menu.GESTION_HABITACIONES:
+				
+				break;
+				
+			case Menu.GESTION_HOTELES:
+				
+				break;
+				
+			case Menu.GESTION_RESERVAS:
+				
+				break;
+			case Menu.SALIR:
+				break;
+			}
+
+		} while (opc != 0);
+	}
+}
