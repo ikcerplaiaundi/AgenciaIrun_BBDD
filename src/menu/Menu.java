59de3759106a22184c9c4f5d57249d1207a52728
Polskivus <polako_irun@icloud.com>
2023-02-07 09:32:46 +0100

    se han creado la idea principal del menu

---
+++ src/menu/Menu.java
@@ -0,0 +1,31 @@
+package menu;
+
+public class Menu {
+
+	public static final int SALIR = 0;
+	/*MENU PRINCIPAL*/
+	public static final int GESTION_CLIENTES = 1;
+	public static final int GESTION_HABITACIONES = 2;
+	public static final int GESTION_HOTELES = 3;
+	public static final int GESTION_RESERVAS = 4;
+	/*MENU CLIENTES*/
+	public static final int INSERTAR_CLIENTE = 1;
+	public static final int ELIMINAR_CLIENTE = 2;
+	public static final int MODIFICAR_CLIENTE = 3;
+	public static final int VISUALIZAR_CLIENTE = 4;
+	/*MENU HOTELES*/
+	public static final int INSERTAR_HOTEL = 1;
+	public static final int ELIMINAR_HOTEL = 2;
+	public static final int MODIFICAR_HOTEL = 3;
+	public static final int VISUALIZAR_HOTEL = 4;
+	/*MENU HABITACIONES*/
+	public static final int VISUALIZAR_HABITACION = 1;
+	public static final int ELIMINAR_HABITACIONES = 2;
+	public static final int MODIFICAR_HABITCION = 3;
+	public static final int INSERTAR_HABITACION = 4;
+	/*MENU RESERVAS*/
+	public static final int VISUALIZAR_RESERVAS = 1;
+	public static final int ELIMINAR_RESERVAS = 2;
+	public static final int MODIFICAR_RESERVAS = 3;
+	public static final int INSERTAR_RESERVAS = 4;
+}
