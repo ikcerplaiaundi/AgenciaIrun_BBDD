e1cb6d7bed14b38d4a2874cacba4c34fd483be85
Polskivus <polako_irun@icloud.com>
2023-02-07 08:56:48 +0100

    CLASE CLIENTES CREADA (igual hay que meter algo mas)

--- src/objetos/Clientes.java
+++ src/objetos/Clientes.java
@@ -2,4 +2,24 @@ package objetos;
 
 public class Clientes {
 
+	String dni;
+	String nombre;
+	String apellido;
+	String direccion;
+	String localidad;
+	
+	public Clientes() {
+		
+	}
+	
+	public Clientes(String dni, String nombre, String apellido, String direccion, String localidad) {
+		super();
+		this.dni = dni;
+		this.nombre = nombre;
+		this.apellido = apellido;
+		this.direccion = direccion;
+		this.localidad = localidad;
+	}
+	
 }
+
