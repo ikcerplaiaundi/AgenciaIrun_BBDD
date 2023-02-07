670cd5538d32b544111f7b13b998738ff360cd67
plaiaundi <plaiaundi@HZ309811.H012108.NET>
2023-02-07 08:50:09 +0100

    habitaciones

---
+++ src/objetos/Habitaciones.java
@@ -0,0 +1,44 @@
+package objetos;
+
+public class Habitaciones {
+public int id ;	
+public int id_hotel ;
+public String numero ;
+public String descripcion ;
+public double precio ;
+public int getId() {
+	return id;
+}
+public void setId(int id) {
+	this.id = id;
+}
+public int getId_hotel() {
+	return id_hotel;
+}
+public void setId_hotel(int id_hotel) {
+	this.id_hotel = id_hotel;
+}
+public String getNumero() {
+	return numero;
+}
+public void setNumero(String numero) {
+	this.numero = numero;
+}
+public String getDescripcion() {
+	return descripcion;
+}
+public void setDescripcion(String descripcion) {
+	this.descripcion = descripcion;
+}
+public double getPrecio() {
+	return precio;
+}
+public void setPrecio(double precio) {
+	this.precio = precio;
+}
+public String toString() {
+	return "Habitaciones [id=" + id + ", id_hotel=" + id_hotel + ", numero=" + numero + ", descripcion=" + descripcion
+			+ ", precio=" + precio + "]";
+}
+
+}
